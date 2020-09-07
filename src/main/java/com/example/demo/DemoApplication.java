4a319fe204e174c87c56f66989aaba0a55280b79
sriya3107 <sriyachandrage.cs16@rvce.edu.in>
2020-09-07 12:36:43 +0530

    Hello World

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		//SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext ctx =  SpringApplication.run(DemoApplication.class, args);
+		HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+		helloWorld.sayHello();
+
 	}
 
 }
